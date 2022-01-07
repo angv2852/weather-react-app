1d1a7e16dfa977c4a46980b6cf6a0027ae2c745e
Ang <angv2852@gmail.com>
2022-01-07 12:53:31 -0700

    Removed practice code to start importing weather app from vanilla

--- src/App.js
+++ src/App.js
@@ -5,19 +5,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
-        <Weather />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,23 +1,13 @@
 import React from "react";
 import axios from "axios";
-import Loader from "react-loader-spinner";
 
 export default function Weather() {
   function handleResponse(response) {
-    alert(`the weather in New York is ${Math.round(response.data.main.temp)}`);
+    return <h2>Weather app</h2>;
+
+    let apiKey = "24938515d6364eea8b6bfd1202de9eb1";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
-
-  let apiKey = "24938515d6364eea8b6bfd1202de9eb1";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <Loader
-      type="Hearts"
-      color="#00BFFF"
-      height={100}
-      width={100}
-      timeout={3000} //3 secs
-    />
-  );
 }
