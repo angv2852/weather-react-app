ba2177d7d11ed9afc46d169b652fa11c31559a23
Ang <angv2852@gmail.com>
2022-01-15 12:37:29 -0700

    Added weather API

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -5,12 +5,13 @@ import axios from "axios";
 
 export default function CurrentWeather(props) {
   const [currentWeather, setCurrentWeather] = useState({ loaded: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function showTemperature(response) {
     setCurrentWeather({
       loaded: true,
       city: response.data.name,
-      iconUrl: "https://openweathermap.org/img/wn/01d@2x.png",
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
@@ -18,18 +19,35 @@ export default function CurrentWeather(props) {
       feelsLike: response.data.main.feels_like,
     });
   }
+  function search() {
+    let apiKey = "3e4837b1fe633f474ee7c985588f6e07";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(showTemperature);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+    // search for a city
+  }
+
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
 
   if (currentWeather.loaded) {
     return (
       <div className="SearchBar">
         <WeatherToday weather={currentWeather} />
-        <form id="search-form">
+        <form onSubmit={handleSubmit} id="search-form">
           <input
             type="text"
             placeholder="Enter city name"
             autoFocus="on"
             autoComplete="off"
             id="city-name-input"
+            onChange={changeCity}
           />
           <input
             type="submit"
@@ -47,11 +65,7 @@ export default function CurrentWeather(props) {
       </div>
     );
   } else {
-    let apiKey = "3e4837b1fe633f474ee7c985588f6e07";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=imperial`;
-
-    axios.get(apiUrl).then(showTemperature);
-
+    search();
     return "Loading...";
   }
 }
